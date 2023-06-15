a053bd8f7fe502a42577613890a1ec289c42fa50
nicolasve18 <132798791+nicolasve18@users.noreply.github.com>
2023-06-14 21:12:07 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='trifasicos',
+    version='1.0.0',
+    author='Nicolas Velasco',
+    author_email='nicolasvelascoalvarez7@gmail.com',
+    description='esta libreria resuelvas sistemas trifasicos',
+    url='https://github.com/nicolasve18/trifasicos',
+    packages=['trifasicos'],
+        classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ]
+   
+)
